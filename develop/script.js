ebcab23af7e14e0200f0fa4df16720029571b318
Pro-Code-art <jayme.coder@gmail.com>
2020-12-14 18:40:17 -0800

    set up minimal js layout for saving city to client-side storage

--- develop/script.js
+++ develop/script.js
@@ -9,3 +9,13 @@ document.querySelector('strong').addEventListener('click', function () {
 
 // OpenWeather API Key
 var apiKey = "8d184f5374e0c6605e52dd1fb2f43631";
+
+
+
+// Saves searched city to local storage
+const searchCityButton = document.getElementById('search-city');
+searchCityButton.addEventListener('click', saveCity);
+function saveCity () {
+    let userCity = document.querySelector('input').value;
+    localStorage.setItem('city', userCity);
+}
