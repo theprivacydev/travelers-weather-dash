2dcaee10331097aad45bfae46c2e021f63d5e5ed
Pro-Code-art <jayme.coder@gmail.com>
2020-12-14 18:25:30 -0800

    initial commit

---
+++ develop/script.js
@@ -0,0 +1,11 @@
+
+// Test functionality of JS file
+document.querySelector('strong').addEventListener('click', function () {
+
+    alert('you clicked me!');
+})
+
+
+
+// OpenWeather API Key
+var apiKey = "8d184f5374e0c6605e52dd1fb2f43631";
