0a8d52f10f41dc2c8a1268a3a421e1e0682ee150
Pro-Code-art <jayme.coder@gmail.com>
2020-12-16 13:33:55 -0800

    created successful api call and displayed response on the page

--- develop/script.js
+++ develop/script.js
@@ -1,17 +1,44 @@
 
 // OpenWeather API Key
-var apiKey = "8d184f5374e0c6605e52dd1fb2f43631";
-
+const apiKey = "8d184f5374e0c6605e52dd1fb2f43631";
 
 
 // Creates event listener for "Go" button
 const searchCityButton = $('#search-city');
 searchCityButton.click(saveCity);
 
-// Saves searched city to local storage
-function saveCity () {
+// Saves searched city to client-side storage
+function saveCity (event) {
+    event.preventDefault();
+
+    getWeatherInfo();
+
     let userCity$ = $('input').val();
     localStorage.setItem('city', userCity$);
+
+    sidebarSavedCities();
 }
 
 
+// Calls weather by the city name and puts info in html
+function getWeatherInfo () {
+    let cityName$ = $('input').val();
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName$ + "&appid=" + apiKey;
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+        $('#city-name-date').text(response.name);
+      });
+}
+
+function sidebarSavedCities () {
+
+
+}
+
+    //   api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+
+    //   api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
