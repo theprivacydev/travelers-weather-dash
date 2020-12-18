8da4af2ebcea87f9219b52452e987ae2292e056d
Pro-Code-art <jayme.coder@gmail.com>
2020-12-17 18:18:57 -0800

    changed loadDashTemplate to opening page function

--- develop/script.js
+++ develop/script.js
@@ -5,13 +5,13 @@ const apiKey = "8d184f5374e0c6605e52dd1fb2f43631";
 // Sets Date
 var currentDate = moment().format("MMM Do YYYY");
 
-loadDashTemplate();
+loadOpeningPage();
 
 // Creates event listener for buttons
 const searchCityButton = $('#search-city');
 searchCityButton.click(saveCity);
 const myCities = $('#saved-cities');
-myCities.click(sidebarSavedCities);
+myCities.click(dropdownMyCities);
 
 // Saves searched city to client-side storage
 function saveCity (event) {
@@ -29,7 +29,7 @@ function kelvinToFarenheight (k) {
 
 // Calls weather by the city name and puts info in html
 function getWeatherInfo () {
-
+    $('h3').hide();
     let cityName$ = $('input').val();
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName$ + "&appid=" + apiKey;
       $.ajax({
@@ -55,11 +55,7 @@ function getWeatherInfo () {
 }
 
 // Displays cities from client-side storage in sidebar
-function sidebarSavedCities () {
-    $('.container').css('margin: 50px');
-
-    // Unhide sidebar
-    let savedCities = $('.sidebar').show();
+function dropdownMyCities () {
 
     // Displays cities in sidebar
     let city = savedCities.text(localStorage.getItem('city'));
@@ -68,26 +64,8 @@ function sidebarSavedCities () {
     
 }
 
-function loadDashTemplate () {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Portland" + "&appid=" + apiKey;
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(data) {
-        console.log(data);
-        let temp = kelvinToFarenheight(data.main.temp);
-        $('#city-name').text(data.name);
-        $('#date').text(currentDate);
-        $('#temperature').text('Temperature: ' + data.main.temp);
-        $('#humidity').text('Humidity: ' + data.main.humidity);
-        $('#wind-speed').text('Wind-speed: ' + data.wind.speed);
-        $('#uv-index').text('UV Index: ');
-
-        // Loading weather icons
-        var iconCode = data.weather[0].icon;
-        var iconSource = "http://openweathermap.org/img/w/" + iconCode + ".png"
-        $('#icon-image').attr('src', iconSource);
-      });
+function loadOpeningPage () {
+   $('#opening').text('Enter a City in the search bar to get the weather!')
 }
 
 function getForecast () {
