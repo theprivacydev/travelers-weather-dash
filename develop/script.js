bcf7a5a5054a474abe8fedae9af8df1b4317d1b6
Pro-Code-art <jayme.coder@gmail.com>
2020-12-20 20:05:25 -0800

    got cities to store inside an array in local storage and created function to pull those cities and append them to the dropdown menu

--- develop/script.js
+++ develop/script.js
@@ -1,17 +1,16 @@
 
 // OpenWeather API Key
-const apiKey = "8d184f5374e0c6605e52dd1fb2f43631";
+const openWeatherApiKey = "8d184f5374e0c6605e52dd1fb2f43631";
+const uvApiKey = "&key=d3f777192d194d2fa2c8955860268325";
 
 // Sets Date
 var currentDate = moment().format("MMM Do YYYY");
 
-loadOpeningPage();
-
+var citiesArr = [];
 // Creates event listener for buttons
 const searchCityButton = $('#search-city');
 searchCityButton.click(getWeatherInfo);
 const myCities = $('#saved-cities');
-myCities.click(dropdownMyCities);
 
 // Converts Temp to farenheight from Kelvin
 function kelvinToFarenheight (k) {
@@ -24,8 +23,9 @@ function getWeatherInfo (event) {
   event.preventDefault();
     $('h3').hide();
     let cityName$ = $('input').val();
-    localStorage.setItem('city', cityName$);
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName$ + "&appid=" + apiKey;
+    citiesArr.push(cityName$);
+    JSON.stringify(localStorage.setItem('TWD Cities: ', citiesArr));
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName$ + "&appid=" + openWeatherApiKey;
       $.ajax({
         url: queryURL,
         method: "GET"
@@ -40,43 +40,72 @@ function getWeatherInfo (event) {
         $('#wind-speed').text('Wind-speed: ' + data.wind.speed + ' mph');
         $('#uv-index').text('UV Index: ');
 
+
+        $('.forecast').addClass('weather-info');
+
+        // let uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + data.lat + "&lon=" + data.lon + "&appid=" + openWeatherApiKey;
+  
+        // $.ajax({
+        //   url: uvURL,
+        //   method: "GET"
+        // }).then(function(response) {
+        //   console.log(response);
+          // $('#uv-index').text('UV Index: ');
+          // });
+
         // Display weather icon to the page
         var iconCode = data.weather[0].icon;
         var iconSource = "http://openweathermap.org/img/w/" + iconCode + ".png";
         $('#icon-image').attr('src', iconSource);
-
+        dropdownMyCities();
       });
 }
 
-// Displays cities from client-side storage in sidebar
+
+// function getUvIndex () {
+//   let uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}"
+  
+//   $.ajax({
+//     url: uvURL,
+//     method: "GET"
+//   }).then(function(data) {
+
+
+//     $('#uv-index').text('UV Index: ');
+//     });
+
+// }
+
+// Displays cities from client-side storage in dropdown menu
 function dropdownMyCities () {
-
-    // Displays cities in sidebar
-    let city = savedCities.text(localStorage.getItem('city'));
-
-    city.click(getWeatherInfo);
-    
+  let savedCites = JSON.parse(localStorage.getItem('TWD Cities: '));
+  console.log(savedCites);
+      for (let i = 0; i < savedCites.length; i++) {
+        let city$ = $('<li>').addClass('dropdown-item dropright');
+        $('.dropdown-menu').append(city$);
+        city$.text(savedCites[i]);
+        console.log(savedCites[i]);
+      }
 }
 
 function loadOpeningPage () {
-  if (localStorage.key === 'city') {
-    getWeatherInfo()
-  }
-   $('#opening').text('Enter a City in the search bar to get the weather!')
-   
+  // if (localStorage.key === 'TWD Cities: ') {
+  //   getWeatherInfo();
+  // } 
+   $('#opening').text('Enter a City in the search bar to get the weather!');
 }
 
-function getForecast () {
-    $('.forecast').addClass('weather-info');
-    let city = "Portland";
-    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-        console.log(response.list[0].dt_txt);
 
-      });
-}
+// function getForecast () {
+//     $('.forecast').addClass('weather-info');
+//     let city = "&city=Portland";
+//     let queryURL = "https://api.weatherbit.io/v2.0/history/daily?" + city + "&start_date=2020-12-16&end_date=2020-12-17" + uvApiKey;
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//       }).then(function(data) {
+//         console.log(data);
+//         $('#uv-index').text('UV Index: ', );
 
-// getForecast();
+//       });
+// }
